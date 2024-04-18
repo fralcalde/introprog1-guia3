cd55065e4bc9365f861256f1cb0625c0557f73cb
Francisco Alcalde <francisco@alcalde.com.ar>
2024-04-17 21:59:51 -0300

    guia4 parte 2

--- guia4.hs
+++ guia4.hs
@@ -49,3 +49,27 @@ cantDigitos :: Integer -> Integer
 cantDigitos n
     | 0 <= n && n < 10 = 1
     | otherwise = 1 + cantDigitos (sacarUnidad n)
+
+-- Ejercicio 13
+dobleSumaDePotencias :: Integer -> Integer -> Integer
+dobleSumaDePotencias 1 m = sumaDePotencias 1 m
+dobleSumaDePotencias n m = sumaDePotencias n m + dobleSumaDePotencias (n - 1) m
+
+sumaDePotencias :: Integer -> Integer -> Integer
+sumaDePotencias n 1 = n
+sumaDePotencias n m = n^m + sumaDePotencias n (m - 1)
+
+-- Ejercicio 16
+-- a)
+menorDivisor :: Integer -> Integer
+menorDivisor 1 = 1
+menorDivisor n = menorDivisorPasoRecursivo n 2
+
+menorDivisorPasoRecursivo :: Integer -> Integer -> Integer
+menorDivisorPasoRecursivo n i
+    | esDivisible n i = i
+    | otherwise = menorDivisorPasoRecursivo n (i + 1)
+
+-- b)
+esPrimo :: Integer -> Bool
+esPrimo n = menorDivisor n == n
