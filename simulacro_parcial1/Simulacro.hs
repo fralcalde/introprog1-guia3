67e192c22d837081b940090e28cdb8a54778de58
Francisco Alcalde <francisco@alcalde.com.ar>
2024-04-24 21:46:58 -0300

    Simulacro de parcial 1

---
+++ simulacro_parcial1/Simulacro.hs
@@ -0,0 +1,62 @@
+module Simulacro where
+
+    -- Ejercicio 1
+    esReflexiva :: (Eq t) => (t, t) -> Bool
+    esReflexiva (a, b)
+        | a == b = True
+        | otherwise = False
+
+    sonEquivalentes :: (Eq t) => (t, t) -> (t, t) -> Bool
+    sonEquivalentes (a, b) (c, d)
+        | (a == c && b == d) || (a == d && b == c) = True
+        | otherwise = False
+
+    inRelaciones :: (Eq t) => (t, t) -> [(t, t)] -> Bool
+    --pertenece :: (Eq t) => t -> [t] -> Bool
+    inRelaciones _ [] = False
+    inRelaciones elem (x:xs) 
+        | sonEquivalentes elem x = True
+        | otherwise = inRelaciones elem xs
+
+    relacionesValidas :: [(String, String)] -> Bool
+    relacionesValidas [] = True
+    relacionesValidas (rel:relaciones)
+        | esReflexiva rel || inRelaciones rel relaciones = False
+        | otherwise = relacionesValidas relaciones
+    
+    -- Ejercicio 2
+    pertenece :: (Eq t) => t -> [t] -> Bool
+    pertenece _ [] = False
+    pertenece elem (x:xs)
+        | elem == x = True
+        | otherwise = pertenece elem xs
+
+    addPersona :: String -> [String] -> [String]
+    addPersona p [] = [p]
+    addPersona p xs
+        | pertenece p xs = xs
+        | otherwise = p:xs
+
+    personas :: [(String, String)] -> [String]
+    personas [] = []
+    personas ((p1, p2):xs) = addPersona p1 (addPersona p2 (personas xs))
+
+    -- Ejercicio 3
+    amigosDe :: String -> [(String, String)] -> [String]
+    amigosDe _ [] = []
+    amigosDe p ((a1, a2):relaciones) 
+        | p == a1 = addPersona (a2) (amigosDe p relaciones)
+        | p == a2 = addPersona (a1) (amigosDe p relaciones)
+        | otherwise = amigosDe p relaciones
+
+    -- Ejercicio 4
+    contarAmigos :: String -> [(String, String)] -> Integer
+    contarAmigos _ [] = 0
+    contarAmigos p ((a1, a2):xs)
+        | p == a1 || p == a2 = 1 + contarAmigos p xs
+        | otherwise = contarAmigos p xs
+
+    --personaConMasAmigos :: [(String, String)] -> String
+    --personasConMasAmigos (p1, p2):xs
+    --    | contarAmigos p1 >= contarAmigos p2 = 
+    --personaConMasAmigos _ = "Yo seguro que no"
---
+++ simulacro_parcial1/test.hs
@@ -0,0 +1,62 @@
+import Test.HUnit
+import Simulacro
+
+main = runTestTT esReflexivaTests
+
+esReflexivaTests = test [
+    "Relación reflexiva" ~: esReflexiva ("a", "a") ~?= True,
+    "Relación no reflexiva" ~: esReflexiva ("a", "b") ~?= False
+    ]
+
+inRelacionesTests = test [
+    "Lista vacía" ~: inRelaciones (1, 2) [] ~?= False,
+    "Elemento no pertenece" ~: inRelaciones (1, 2) [(5, 3), (4, 6)] ~?= False,
+    "Elemento si pertenece" ~: inRelaciones (1, 2) [(1, 2)] ~?= True,
+    "Elemento equivalente pertenece" ~: inRelaciones (1, 2) [(2, 1)] ~?= True
+    ]
+
+ningunaRelacion = []
+
+todasValidas = [
+    ("u1", "u2"),
+    ("u3", "u2"),
+    ("u3", "u1")
+    ]
+
+contieneNoValidas = [
+    ("u2", "u2")
+    ]
+
+contieneRepetidas = [
+    ("u2", "u3"),
+    ("u2", "u3")
+    ]
+
+contieneEquivalentes = [
+    ("u1", "u2"),
+    ("u2", "u1")
+    ]
+
+relacionesValidasTests = test [
+    "Lista vacía" ~: relacionesValidas ningunaRelacion ~?= True,
+    "Todas válidas" ~: relacionesValidas todasValidas ~?= True,
+    "Contiene no válidas" ~: relacionesValidas contieneNoValidas ~?= False,
+    "Contiene repetidas" ~: relacionesValidas contieneRepetidas ~?= False,
+    "Contiene equivalentes" ~: relacionesValidas contieneEquivalentes ~?= False
+    ]
+
+personasCaso2 = [
+    ("Joe", "Frank"),
+    ("Laura", "Beethoven"),
+    ("Laura", "Travis")
+    ]
+
+personasTests = test [
+    "Lista vacía" ~: personas ningunaRelacion ~?= [],
+    "Caso 1" ~: personas todasValidas ~?= ["u2", "u3", "u1"],
+    "Caso 2" ~: personas personasCaso2 ~?= ["Joe", "Frank", "Beethoven", "Laura", "Travis"]
+    ]
+
+amigosDeTests = test [
+    "Amigos de Laura" ~: amigosDe "Laura" personasCaso2 ~?= ["Beethoven", "Travis"]
+    ]
